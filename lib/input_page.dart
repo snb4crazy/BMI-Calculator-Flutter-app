f1f6bc8b6b0d4925c5a4973998bb8c0b4cc40687
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-15 16:14:58 -0400

    InputPage: Refactoring

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:bmi_calculator/reusable_card.dart';
+import 'gender_icons.dart';
 
 const bottonContainerHeight = 80.0;
 const reusableCardColor = Color(0xFF1D1E33);
@@ -71,55 +73,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color colour;
-  final Widget cardChild;
-
-  ReusableCard({@required this.colour, this.cardChild});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: this.colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
-
-class GenderIcons extends StatelessWidget {
-  final IconData genderIcon;
-  final String gender;
-
-  GenderIcons({this.genderIcon, this.gender});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          genderIcon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          gender,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E88),
-          ),
-        )
-      ],
     );
   }
 }
