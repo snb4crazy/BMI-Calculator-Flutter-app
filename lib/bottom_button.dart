3d4dcec3d48ccb11e9e0a3548cc0998788bc4157
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-19 01:02:39 -0400

    Refactoring Bottom Button Widget

---
+++ lib/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.onTap, @required this.title});
+
+  final Function onTap;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            title,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
