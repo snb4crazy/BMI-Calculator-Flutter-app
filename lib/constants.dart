bc8cab845fb153b6587ba6aa9b3be88b8304ba0f
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-19 01:03:45 -0400

    Constants: Results Page styling

--- lib/constants.dart
+++ lib/constants.dart
@@ -16,3 +16,17 @@ const kLargeButtonTextStyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const kResultTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Color(0xFF24D876),
+  fontWeight: FontWeight.bold,
+);
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  color: Color(0xFF24D876),
+  fontWeight: FontWeight.bold,
+);
