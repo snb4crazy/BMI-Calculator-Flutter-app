03b6a8f9c3d46fb19f4df66e1cddf05caad6be0a
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-19 01:04:24 -0400

    Results Page Design

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:bmi_calculator/reusable_card.dart';
+import 'bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -7,7 +10,49 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Text('Todo put results'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text('Your results', style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '100.50',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your body result, you should eat more',
+                    style: kResultTextStyle,
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              title: 'RE-CALCULATE')
+        ],
+      ),
     );
   }
 }
